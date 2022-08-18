2ded0cc4d65483e70422216d5d9241ff9643d126
hauk0101 <yaoqiao@allinmd.cn>
2022-08-18 09:56:27 +0800

    提交bug复现代码

---
+++ src/pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <view class="index">
+    <template v-if="item.patientInfo !== 1">hahaha</template>
+    <text>{{ msg }}</text>
+    <textarea
+      :value="keyword"
+      type="text"
+      style="height:28px;"
+    />
+  </view>
+</template>
+
+<script>
+import './index.scss'
+
+export default {
+  data () {
+    return {
+      msg: 'Hello world!',
+      item:{
+        patientInfo: 0
+      },
+      keyword:''
+    }
+  }
+}
+</script>
